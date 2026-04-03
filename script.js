9dbd91104ed1be81d50ebfdefab4f4dcf8bb7ae0
Shreya Midya <129717813+Shreya211149@users.noreply.github.com>
2026-04-04 00:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let p=document.getElementById("counter");
+let button=document.getElementById("btn");
+let count=0;
+
+button.addEventListener("click",function(){
+	alert(count);
+	count++;
+	p.innerText=count;
+});
